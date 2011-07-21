e00e7e5b5c6e72b5c4b7713594bcfd7829e6e789
Tom Bombadil <amanibhavam@destructuring.org>
2011-07-21 16:05:50 +0000

    port conflict between natty/lucid during veewee install

--- definitions/natty/definition.rb
+++ definitions/natty/definition.rb
@@ -4,7 +4,7 @@ Veewee::Session.declare({
   :os_type_id => 'Ubuntu', :iso_file => "ubuntu-11.04-server-i386.iso",
   :boot_wait => "0", :boot_cmd_sequence => [ ],
   :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",
-  :ssh_host_port => "7222", :ssh_guest_port => "22",
+  :ssh_host_port => "7223", :ssh_guest_port => "22",
   :sudo_cmd => "bash '%f'",
   :shutdown_cmd => "shutdown -P now",
   :postinstall_files => [ "postinstall.sh" ], :postinstall_timeout => "10000"

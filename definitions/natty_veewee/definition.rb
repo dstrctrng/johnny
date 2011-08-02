8fae0d988880f2062cb9ed7aadacaa9e75415191
Tom Bombadil <amanibhavam@destructuring.org>
2011-08-02 02:20:00 +0000

    retrieve Ubuntu ISOs and unpack

--- definitions/natty_veewee/definition.rb
+++ definitions/natty_veewee/definition.rb
@@ -2,6 +2,8 @@ Veewee::Session.declare({
   :cpu_count => '1', :memory_size=> '512',
   :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
   :os_type_id => 'Ubuntu', :iso_file => "ubuntu-11.04-server-i386.iso",
+  :iso_src => "http://releases.ubuntu.com/11.04/ubuntu-11.04-server-i386.iso",
+  :iso_md5 => "ce1cee108de737d7492e37069eed538e",
   :boot_wait => "0", :boot_cmd_sequence => [ ],
   :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",

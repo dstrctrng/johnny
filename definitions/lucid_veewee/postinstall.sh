bb0a0a22be003650d02a2c46872b064926691371
Tom Bombadil <amanibhavam@destructuring.org>
2011-08-04 02:41:36 +0000

    remove udev directories

--- definitions/lucid_veewee/postinstall.sh
+++ definitions/lucid_veewee/postinstall.sh
@@ -5,10 +5,10 @@
 export DEBIAN_FRONTEND="noninteractive"
 
 # udev cleanup
-rm -f /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /etc/udev/rules.d/70-persistent-net.rules
 mkdir -p /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
-rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
+rm -rf /lib/udev/rules.d/75-persistent-net-generator.rules
 
 # dhcp cleanup
 rm -f /var/lib/dhcp3/*

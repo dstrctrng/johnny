fbae83fb742c6343ab229cc02ae809580d68eeaf
Tom Bombadil <amanibhavam@destructuring.org>
2012-02-22 22:21:28 +0000

    update lucid iso, dont run custom scripts, remove PANCAKE env vars

--- definitions/lucid_veewee/definition.rb
+++ definitions/lucid_veewee/definition.rb
@@ -1,8 +1,8 @@
 Veewee::Session.declare({
   :cpu_count => '1', :memory_size=> '512',
   :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
-  :os_type_id => 'Ubuntu', :iso_file => "ubuntu-10.04.3-server-i386.iso",
-  :iso_src => "http://releases.ubuntu.com/10.04.3/ubuntu-10.04.3-server-i386.iso",
+  :os_type_id => 'Ubuntu', :iso_file => "ubuntu-10.04.4-server-amd64.iso",
+  :iso_src => "http://releases.ubuntu.com/lucid/ubuntu-10.04.4-server-amd64.iso",
   :boot_wait => "0", :boot_cmd_sequence => [ ],
   :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",

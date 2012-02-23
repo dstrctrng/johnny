b817b0044214db7379d3c66d4ade2d4b8d3b7885
Tom Bombadil <amanibhavam@destructuring.org>
2012-02-23 01:38:07 +0000

    cleaning script: less indent for stage 2 remote run

--- definitions/lucid_veewee/postinstall.sh
+++ definitions/lucid_veewee/postinstall.sh
@@ -2,6 +2,7 @@
 
 # most of this script taken from veewee, vagrant
 
+# dont prompt
 export DEBIAN_FRONTEND="noninteractive"
 
 # udev cleanup
@@ -15,8 +16,7 @@ rm -f /var/lib/dhcp3/*
 
 if [[ -z $1 ]]; then # STAGE 1: during veewee build, and copied to ~/ so stage 2 can be called during vagrant
   # vagrant ssh key
-  mkdir -p ~/.ssh
-  chmod 700 ~/.ssh
+  install -v -d -m 0700 ~/.ssh
   cat > ~/.ssh/authorized_keys << EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
 EOF
@@ -28,18 +28,22 @@ EOF
 
   # dhcp delays
   echo "pre-up sleep 2" >> /etc/network/interfaces
-else # STAGE 2: during vagrant after rebooting into updated kernel
 
-  # build requirements for virtual box guest additions
-  aptitude install -y build-essential wget linux-headers-$(uname -r)
-
-  VBOX_VERSION=$(cat ~/.vbox_version)
-  pth_vguest=$(mktemp)
-  wget -O $pth_vguest http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
-  mount -o loop $pth_vguest /mnt
-  sh /mnt/VBoxLinuxAdditions.run
-  umount /mnt
-
-  rm $pth_vguest
+  exit
 fi
 
+# STAGE 2: during vagrant after rebooting into updated kernel
+
+# build requirements for virtual box guest additions
+aptitude install -y build-essential wget linux-headers-$(uname -r)
+
+tmp_vguest=$(mktemp -t XXXXXXXXX)
+
+VBOX_VERSION=$(cat ~/.vbox_version)
+wget -O $tmp_vguest http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+
+mount -o loop $tmp_vguest /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+rm $tmp_vguest

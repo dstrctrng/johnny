581982073beb85f9ae8918e3a2d06ce189f68cb1
Tom Bombadil <amanibhavam@destructuring.org>
2012-02-24 00:52:27 +0000

    more generic postinstall, no rvm try to use system ruby

--- definitions/lucid/vagrant/postinstall.sh
+++ definitions/lucid/vagrant/postinstall.sh
@@ -29,20 +29,6 @@ umount /mnt
 
 rm $tmp_vguest
 
-# update OS packages
-if [[ -z $(grep multiverse /etc/apt/sources.list) ]]; then
-  sed -i s/universe/"universe multiverse"/ /etc/apt/sources.list
-fi
-
-# relocation large dirs to /data
-install -d -o root -g root -m 0755 /data/home
-
-if [[ ! -L /home ]]; then
-  rsync -a /home/ /data/home/
-  rm -r /home
-  ln -s /data/home /
-fi
-
 # grant sudo permissions
 cat > /etc/sudoers <<EOF
 Defaults env_reset
@@ -51,57 +37,20 @@ EOF
 
 chmod 440 /etc/sudoers
 
-aptitude update -q
-aptitude full-upgrade -q -y -o Aptitude::CmdLine::Ignore-Trust-Violations=true
-aptitude install -y rsync figlet -o Aptitude::CmdLine::Ignore-Trust-Violations=true
+# update OS packages
+if [[ -z $(grep multiverse /etc/apt/sources.list) ]]; then
+  sed -i s/universe/"universe multiverse"/ /etc/apt/sources.list
+fi
+
+# install the basics
+aptitude install -q -y rsync figlet 
 
 # install build packages
-figlet "ruby requirements"
-aptitude install -q -y build-essential autoconf bison curl git-core -o Aptitude::CmdLine::Ignore-Trust-Violations=true
-aptitude install -q -y libreadline5 libreadline5-dev zlib1g zlib1g-dev openssl libssl-dev libsqlite3-0 sqlite3 libsqlite3-dev libxml2-dev libsasl2-dev libxslt-dev -o Aptitude::CmdLine::Ignore-Trust-Violations=true
+figlet "ruby"
+aptitude install -q -y ruby rubygems
 
-# install rvm
-figlet "rvm"
-install -d -o root -g root -m 0755  /usr/local/bin
-tmp_rvm=$(mktemp -t XXXXXXXXX)
-wget --no-check-certificate https://github.com/zendesk/rvm/raw/master/contrib/install-system-wide -O $tmp_rvm
-bash $tmp_rvm
-rm -f $tmp_rvm
-install -d -o root -g root -m 0755  /usr/local/bin
-
-# source rvm, use ree
-rm -f /etc/profile.d/rvm.sh
-touch /etc/profile.d/rvm.sh
-for a in /root/.bashrc; do
-  touch $a
-  echo "[[ -f /usr/local/lib/rvm ]] && source /usr/local/lib/rvm" >> $a
-  echo "rvm ree" >> $a
-done
-
-# rvm ops, don't mess with error handling
-export RUBY_GC_MALLOC_LIMIT=60000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_HEAP_MIN_SLOTS=500000
-export RUBY_HEAP_SLOTS_INCREMENT=1
-
-want_187="2011.03"
-
-set +e
-source /usr/local/lib/rvm
-
-ncpus=$(grep -c "^processor" /proc/cpuinfo)
-export rvm_make_flags="-j ${ncpus}"
-
-rvm list strings | grep -q "ree-1.8.7-$want_187" || rvm install ree-1.8.7-$want_187
-
-# upgrade ree rubygems
-rvm ree-1.8.7-$want_187
-rvm rubygems 1.5.3
-
-# some global gems
-rvm exec gem install bundler -v1.0.21
-
-# make system ruby the default, here for documentation
-rvm system --default
-
-set -e
+# upgrade rubygems
+gem install rubygems-update -v 1.5.3
+cd /var/lib/gems/1.8/gems/rubygems-update-1.5.3
+ruby setup.rb
+gem uninstall rubygems-update -x -a || true

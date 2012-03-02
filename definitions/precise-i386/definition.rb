d5e89c79429b16d50b7508a185236f6e85cccfd6
Tom Bombadil <amanibhavam@destructuring.org>
2012-03-02 23:05:28 +0000

    adding precise pangolin i386

---
+++ definitions/precise-i386/definition.rb
@@ -0,0 +1,12 @@
+Veewee::Session.declare({
+  :cpu_count => '1', :memory_size=> '512',
+  :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
+  :os_type_id => 'Ubuntu', :iso_file => "ubuntu-12.04-beta1-server-i386.iso",
+  :iso_src => "http://releases.ubuntu.com/precise/ubuntu-12.04-beta1-server-i386.iso",
+  :boot_wait => "0", :boot_cmd_sequence => [ ],
+  :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",
+  :ssh_host_port => "7222", :ssh_guest_port => "22",
+  :sudo_cmd => "bash '%f'",
+  :shutdown_cmd => "shutdown -P now",
+  :postinstall_files => [ "postinstall.sh" ], :postinstall_timeout => "10000"
+})

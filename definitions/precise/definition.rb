95ea29b40ee043c95f9bcb18083a7fef92cb4e6a
Tom Bombadil <amanibhavam@destructuring.org>
2012-05-15 14:40:42 -0700

    precise is out of beta

--- definitions/precise/definition.rb
+++ definitions/precise/definition.rb
@@ -1,8 +1,8 @@
 Veewee::Session.declare({
   :cpu_count => '1', :memory_size=> '512',
   :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
-  :os_type_id => 'Ubuntu', :iso_file => "ubuntu-12.04-beta1-server-amd64.iso",
-  :iso_src => "http://releases.ubuntu.com/precise/ubuntu-12.04-beta1-server-amd64.iso",
+  :os_type_id => 'Ubuntu', :iso_file => "ubuntu-12.04-server-amd64.iso",
+  :iso_src => "http://releases.ubuntu.com/precise/ubuntu-12.04-server-amd64.iso",
   :boot_wait => "0", :boot_cmd_sequence => [ ],
   :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",

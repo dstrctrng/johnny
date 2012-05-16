c6002f6cf42f4aa3174465dfc44d0916b1130400
Tom Bombadil <amanibhavam@destructuring.org>
2012-05-16 10:02:56 -0700

    unattended iso in iso instead of mnt, because veewee expects it in iso/

--- definitions/precise/definition.rb
+++ definitions/precise/definition.rb
@@ -1,7 +1,7 @@
 Veewee::Session.declare({
   :cpu_count => '1', :memory_size=> '512',
   :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
-  :os_type_id => 'Ubuntu', :iso_file => "ubuntu-12.04-server-amd64.iso",
+  :os_type_id => 'Ubuntu', :iso_file => "ubuntu-12.04-server-amd64-unattended.iso",
   :iso_src => "http://releases.ubuntu.com/precise/ubuntu-12.04-server-amd64.iso",
   :boot_wait => "0", :boot_cmd_sequence => [ ],
   :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",

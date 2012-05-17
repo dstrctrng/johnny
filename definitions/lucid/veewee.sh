056f98d64efc0bd53f4bec4dc05b264c317a0ef4
Tom Bombadil <amanibhavam@destructuring.org>
2012-05-17 01:53:42 -0700

    let chef install ssh keys, just get the vagrant key in bootstrap

--- definitions/lucid/veewee.sh
+++ definitions/lucid/veewee.sh
@@ -1,47 +1,26 @@
-#!/biin/bash -e
+#!/bin/bash -e
 
 # most of this script taken from veewee, vagrant
 umask 022
 
 # dont prompt
 export DEBIAN_FRONTEND="noninteractive"
-
-# vagrant ssh key
-install -v -d -m 0700 ~/.ssh
-cat > ~/.ssh/authorized_keys << EOF
-ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
-EOF
+export http_proxy="http://$(echo $SSH_CONNECTION | cut -d= -f2 | awk '{print $1}'):3128"
 
 # update packages
-aptitude install -q -y figlet
-figlet "packages"
 if [[ -z $(grep multiverse /etc/apt/sources.list) ]]; then
   sed -i s/universe/"universe multiverse"/ /etc/apt/sources.list
 fi
-
+aptitude install -q -y figlet
 aptitude update
 aptitude install -q -y wget rsync
-aptitude upgrade -y
-aptitude search -F '%c %p'  linux-image-[0123456789] | grep ^i | awk '{print $2}' | cut -d- -f3- | sed 's#^#linux-headers-#' | xargs aptitude install -q -y
-aptitude clean
-
-# upgrade rubygems
-figlet "rubygems"
+aptitude search -F '%c %p' linux-image-[0123456789] | grep ^i | awk '{print $2}' | cut -d- -f3- | sed 's#^#linux-headers-#' | xargs aptitude install -q -y
 aptitude install -q -y ruby rubygems 
-gem install rubygems-update -v 1.8.17
-cd /var/lib/gems/1.8/gems/rubygems-update-1.8.17
-ruby setup.rb
-gem uninstall rubygems-update -x -a || true
-
-figlet "cleaning"
-# package cleanup
+aptitude upgrade -y
 aptitude clean
 
-# udev cleanup
-rm -rf /etc/udev/rules.d/70-persistent-net.rules
-mkdir -p /etc/udev/rules.d/70-persistent-net.rules
-rm -rf /dev/.udev/
-rm -rf /lib/udev/rules.d/75-persistent-net-generator.rules
+# ssh key
+mkdir -p ~/.ssh
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' > ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
 
-# dhcp cleanup
-rm -f /var/lib/dhcp3/*

e3d3787a6d32ae6f035a3befdbfcd67a1793b53c
Tom Bombadil <amanibhavam@destructuring.org>
2012-05-16 19:21:59 -0700

    handle ssh keys in bin/postinstall clean up network files in vagrant postinstall dont set hostname or sudo rules

--- definitions/dev-precise/postinstall.sh
+++ definitions/dev-precise/postinstall.sh
@@ -5,17 +5,6 @@ nm_instance=$1; shift
 # most of this script taken from veewee, vagrant
 umask 022
 
-# dont prompt
-export DEBIAN_FRONTEND="noninteractive"
-
-# identity
-echo "127.0.0.1 localhost $nm_instance" > /etc/hosts
-echo $nm_instance > /etc/hostname
-hostname $nm_instance
-
-# access
-cat /vagrant/.ssh_authorized_keys >> ~/.ssh/authorized_keys
-
 # udev cleanup
 rm -rf /etc/udev/rules.d/70-persistent-net.rules
 mkdir -p /etc/udev/rules.d/70-persistent-net.rules
@@ -24,11 +13,3 @@ rm -rf /lib/udev/rules.d/75-persistent-net-generator.rules
 
 # dhcp cleanup
 rm -f /var/lib/dhcp3/*
-
-# grant sudo permissions
-cat > /etc/sudoers <<EOF
-Defaults env_reset
-root ALL=(ALL) ALL
-EOF
-
-chmod 440 /etc/sudoers
--- definitions/precise/postinstall.sh
+++ definitions/precise/postinstall.sh
@@ -1,16 +1,10 @@
-#!/biin/bash -e
+#!/bin/bash -e
 
 # most of this script taken from veewee, vagrant
 umask 022
 
 # dont prompt
 export DEBIAN_FRONTEND="noninteractive"
-
-# vagrant ssh key
-install -v -d -m 0700 ~/.ssh
-cat > ~/.ssh/authorized_keys << EOF
-ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
-EOF
 
 # update packages
 aptitude install -q -y figlet
@@ -23,22 +17,12 @@ aptitude update
 aptitude install -q -y wget rsync
 aptitude upgrade -y
 aptitude search -F '%c %p'  linux-image-[0123456789] | grep ^i | awk '{print $2}' | cut -d- -f3- | sed 's#^#linux-headers-#' | xargs aptitude install -q -y
-aptitude clean
 
-# install bundlwr
+# install bundler
 figlet "ruby, bundler"
 aptitude install -q -y ruby rubygems 
 gem install bundler -v 1.1.3
 
-figlet "cleaning"
 # package cleanup
+figlet "cleaning"
 aptitude clean
-
-# udev cleanup
-rm -rf /etc/udev/rules.d/70-persistent-net.rules
-mkdir -p /etc/udev/rules.d/70-persistent-net.rules
-rm -rf /dev/.udev/
-rm -rf /lib/udev/rules.d/75-persistent-net-generator.rules
-
-# dhcp cleanup
-rm -f /var/lib/dhcp3/*

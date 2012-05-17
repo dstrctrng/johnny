d9584268f0814ea3fa6bfa7f6b4ba833ccee275d
Tom Bombadil <amanibhavam@destructuring.org>
2012-05-17 00:36:59 -0700

    dont install bundler, ust packages, through a proxy

--- definitions/precise/veewee.sh
+++ definitions/precise/veewee.sh
@@ -5,26 +5,16 @@ umask 022
 
 # dont prompt
 export DEBIAN_FRONTEND="noninteractive"
+export http_proxy="$(echo $SSH_CONNECTION | cut -d= -f2 | awk '{print $1}'):3128"
 
 # update packages
 if [[ -z $(grep multiverse /etc/apt/sources.list) ]]; then
   sed -i s/universe/"universe multiverse"/ /etc/apt/sources.list
 fi
 aptitude install -q -y figlet
-
-figlet "packages"
 aptitude update
 aptitude install -q -y wget rsync
 aptitude search -F '%c %p' linux-image-[0123456789] | grep ^i | awk '{print $2}' | cut -d- -f3- | sed 's#^#linux-headers-#' | xargs aptitude install -q -y
-
-# install bundler
-figlet "ruby, bundler"
 aptitude install -q -y ruby rubygems 
-gem install bundler -v 1.1.3
-
-figlet "upgrade"
 aptitude upgrade -y
-
-# package cleanup
-figlet "cleaning"
 aptitude clean

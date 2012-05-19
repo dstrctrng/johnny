472e38a7283b98ec10e438607640e19d7f09d030
Tom Bombadil <amanibhavam@destructuring.org>
2012-05-19 08:58:35 -0700

    make lucid definition look like precise

--- definitions/lucid/vagrant/vagrant.sh
+++ definitions/lucid/vagrant/vagrant.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -e
 
+# proxy
+export http_proxy="http://$(echo $SSH_CONNECTION | cut -d= -f2 | awk '{print $1}'):3128"
+
+# vbox guest additions
+ver_virtualbox="$(cat .vbox_version)"
+url_guestadditions="http://download.virtualbox.org/virtualbox/$ver_virtualbox/VBoxGuestAdditions_$ver_virtualbox.iso"
+pth_guestadditions="$HOME/VBoxGuestAdditions_$ver_virtualbox.iso"
+wget -O "$pth_guestadditions" "$url_guestadditions"
+mount -o loop "$pth_guestadditions" /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -f "$pth_guestadditions"
+
 # udev cleanup
 rm -rf /etc/udev/rules.d/70-persistent-net.rules
 mkdir -p /etc/udev/rules.d/70-persistent-net.rules

0803093172eb1b773081904846ef643c0737890d
Tom Bombadil <amanibhavam@destructuring.org>
2012-05-21 12:55:13 -0700

    nodes represent vagrant baseboxes, boxes, instances

---
+++ nodes/destructuring.rb
@@ -0,0 +1,5 @@
+platform "generic"
+platform_version "0.0.1"
+run_list %w(vagrant::box)
+
+vagrnt_box "precise"
--- nodes/precise.rb
+++ nodes/precise.rb
@@ -1,3 +1,3 @@
 platform "generic"
 platform_version "0.0.1"
-run_list %w(vagrant)
+run_list %w(vagrant::basebox)

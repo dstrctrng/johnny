4893012eb1f2c8593bc888a57aaa3dff7a1a0b25
Tom Bombadil <amanibhavam@destructuring.org>
2012-07-01 20:32:10 -0700

    disable proxy since veewee may have built a box with proxy

--- definitions/dev1/provision.sh
+++ definitions/dev1/provision.sh
@@ -4,10 +4,11 @@ if [[ ! -d "/vagrant" ]]; then exit 0; fi
 
 cd /vagrant/provision
 
+perl -pe 's{^(\s*Acquire::http::Proxy)}{#$1}' -i /etc/apt/apt.conf
+
 if [[ ! -x "$(which bundle 2>&-)" ]]; then
   gem install 'bundler-1.1.3.gem'
 fi
-
 bundle check || bundle --local --path vendor/bundle
 
 bundle exec chef-solo -c "config/solo.rb" -N localhost

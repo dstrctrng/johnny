14a28138dd6dce4acbe1ddde3717e63765337337
Tom Bombadil <amanibhavam@destructuring.org>
2012-07-01 20:55:56 -0700

    put linux-server linux-headers-server packages on hold since we dont want to recompile vbox guest additions

--- definitions/destructuring/provision.sh
+++ definitions/destructuring/provision.sh
@@ -6,6 +6,8 @@ cd /vagrant/provision
 
 perl -pe 's{^(\s*Acquire::http::Proxy)}{#$1}' -i /etc/apt/apt.conf
 
+aptitude hold linux-server linux-headers-server
+
 if [[ ! -x "$(which bundle 2>&-)" ]]; then
   gem install 'bundler-1.1.3.gem'
 fi

1c757d0b0301a0705d424e2a6a463e546ec0cd32
Tom Bombadil <amanibhavam@destructuring.org>
2012-08-09 22:24:06 -0700

    load definition.yml

--- definitions/precise/definition.rb
+++ definitions/precise/definition.rb
@@ -1,13 +1,2 @@
-ubuntu = "ubuntu-12.04-server-amd64"
-Veewee::Session.declare({
-  :cpu_count => '1', :memory_size=> '512',
-  :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
-  :os_type_id => 'Ubuntu', :iso_file => "#{ubuntu}-unattended.iso",
-  :iso_src => "http://releases.ubuntu.com/lucid/#{ubuntu}.iso",
-  :boot_wait => "0", :boot_cmd_sequence => [ ],
-  :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",
-  :ssh_host_port => "7222", :ssh_guest_port => "22",
-  :sudo_cmd => "bash '%f'",
-  :shutdown_cmd => "shutdown -P now",
-  :postinstall_files => [ "veewee.sh" ], :postinstall_timeout => "10000"
-})
+require 'yaml'
+Veewee::Session.declare(YAML.load(File.read(File.join(File.dirname(File.expand_path(__FILE__)), "definition.yml"))))

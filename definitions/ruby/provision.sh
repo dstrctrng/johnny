9daf279b612a4a4910199a4941fb78f465d7e106
Tom Bombadil <amanibhavam@destructuring.org>
2012-08-10 01:59:32 -0700

    vagrant.s is one time init, provision.sh is anytime

--- definitions/ruby/provision.sh
+++ definitions/ruby/provision.sh
@@ -1,15 +1,5 @@
 #!/bin/bash -e
 
-export DEBIAN_FRONTEND=noninteractive
-
-aptitude install -y -o Aptitude::CmdLine::Ignore-Trust-Violations=true mysql-client mysql-server tzdata
-mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
-perl -pe 'm{bind-address} && s{127.0.0.1}{0.0.0.0}' -i /etc/mysql/my.cnf 
-echo "update user set host='%' where host='$(hostname -s)'" | mysql -u root mysql
-service mysql restart
-
-aptitude install -y -o Aptitude::CmdLine::Ignore-Trust-Violations=true memcached nginx bind9
-
 cd /vagrant/provision
 bundle check || bundle --local --path vendor/bundle
 bundle exec chef-solo -c "config/solo.rb" -N localhost
--- definitions/ruby/vagrant.sh
+++ definitions/ruby/vagrant.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 # update packages
 aptitude update
 aptitude upgrade -q -y
@@ -11,6 +13,16 @@ gem install rubygems-update
 cd /var/lib/gems/1.8/gems/rubygems-update-*
 ruby setup.rb
 gem install bundler
+
+# install mysql
+aptitude install -y -o Aptitude::CmdLine::Ignore-Trust-Violations=true mysql-client mysql-server tzdata
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
+perl -pe 'm{bind-address} && s{127.0.0.1}{0.0.0.0}' -i /etc/mysql/my.cnf 
+echo "update user set host='%' where host='$(hostname -s)'" | mysql -u root mysql
+service mysql restart
+
+# install other daemons
+aptitude install -y -o Aptitude::CmdLine::Ignore-Trust-Violations=true memcached nginx bind9
 
 # aptitude cleanup
 aptitude clean
